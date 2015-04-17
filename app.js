7e6a0b5b4a34887b1812439125df7e387715c468
teweiliou <davidliou.cs@gmail.com>
2015-04-16 23:43:59 -0500

    Ignore .idea

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var port = process.env.PORT || 8000;
+var google = require('googleapis');
+
 
 var app = express();
 
 app.get('/', function(req,res){
-
 
     res.send("Hello World");
 
 
 });
 
-app.get('/test1', function(req,res){
-
-    res.send("Test 1");
-
-
-});
 
 
 app.listen(port);
